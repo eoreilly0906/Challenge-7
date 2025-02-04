010f1aec03efc3d53f2d86379125294c0bd215c6
Edward O <edwardoreilly0906@gmail.com>
2025-02-03 20:04:26 -0600

    Fixed error in model

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,76 +4,70 @@
 import fs from "fs"
 
 function renderLicenseBadge(license) {
-  if (license === 'Apache 2.0') {
-    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+  if (license === 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+  } else if (license === 'Apache 2.0') {
+    return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
   }
   return '';
-}
-if (license === 'MIT') {
-  return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === "MIT") {
-    return `[MIT](https://opensource.org/licenses/MIT)`;
+  if (license === 'MIT') {
+    return 'https://opensource.org/licenses/MIT';
+  } else if (license === 'Apache 2.0') {
+    return 'https://opensource.org/licenses/Apache-2.0';
   }
-  if (license === "APACHE") {
-    return `[Apache](https://opensource.org/licenses/Apache-2.0)`;
-  }
+  return '';
 }
+
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  let path = "";
-  if (license === "MIT") {
-    path = "utils/mit.md";
+  if (license === 'None') {
+    return '';
   }
-  }
-  if (license === "APACHE") {
-    path = "utils/Apache.md";
-  }
-  const text = fs.readFileSync(path, "utf-8");
-  return text;
+  return `## License
+This project is licensed under the ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(answers) {
-  const licenseBadge = renderLicenseBadge(answers.license);
-  
-  return `# ${answers.projectTitle}
-
-## License
-${answers.license}
-${licenseBadge}
+function generateMarkdown(data) {
+  return `# ${data.projectTitle}
+${renderLicenseBadge(data.license)}
 
 ## Description
-${answers.description}
+${data.description}
 
 ## Table of Contents
-- [Installation](#installation)
-- [Usage](#usage)
-- [License](#license)
-- [Contributing](#contributing)
-- [Tests](#tests)
-- [Questions](#questions)
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
 ## Installation
-${answers.installation}
+${data.installation}
 
 ## Usage
-${answers.usage}
+${data.usage}
+
+${renderLicenseSection(data.license)}
 
 ## Contributing
-${answers.contributing}
+${data.contributing}
 
 ## Tests
-${answers.tests}
+${data.tests}
 
 ## Questions
 For any questions, please contact me:
-- GitHub: [${answers.github}](https://github.com/${answers.github})
-- Email: ${answers.email}`;
+* GitHub: [${data.github}](https://github.com/${data.github})
+* Email: ${data.email}
+`;
 }
 
 export default generateMarkdown;
